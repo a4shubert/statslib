ab4c4ce459ac7d93d0fdfc43d26a024bc5c231af
Alexander Shubert <ashubertt@gmail.com>
2021-03-15 13:53:58 +0300

    updated for package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="statslib",
+    name="stats-lib",
     version="0.0.1",
     author="Alexander Shubert",
     author_email="ashubertt@gmail.com",
