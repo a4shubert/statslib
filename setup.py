0892b228cb5b42cea89b26312b9646d493285f5f
Alexander <alexander.shubert@gazprom-mt.com>
2021-05-13 10:18:04 +0100

    updated for setup.py

--- setup.py
+++ setup.py
@@ -16,5 +16,15 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3"
     ],
-    include_package_data=True
+    include_package_data=True,
+	install_requires=[
+	'matplotlib==3.3.4',
+	'pandas==1.1.5',
+	'seaborn==0.11.1',
+	'statsmodels==0.12.2',
+	'tensorflow==2.4.1',
+	'openpyxl==3.0.7',
+	'scikit-learn==0.24.2',
+	'beautifulsoup4==4.9.3',
+]
 )
